c9ee9d439dfd6f6aedf2118326e037742fc6b3b3
Makoto Aoyama <m@aoyama.dev>
2023-07-10 17:19:35 +0900

    Initial Commit

---
+++ SampleSkipUnspecifiedVersion/SampleSkipUnspecifiedVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSkipUnspecifiedVersionApp.swift
+//  SampleSkipUnspecifiedVersion
+//
+//  Created by Makoto Aoyama on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct SampleSkipUnspecifiedVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
